e7f966db579425974d84e629081841e48d40fe2c
Kazimierz Sroka <kazz.sroka@gmail.com>
2017-12-17 00:42:59 +0100

    fixup! Add handlers and logging

--- nokia_homework.py
+++ nokia_homework.py
@@ -12,29 +12,39 @@ app = Flask(__name__)
 def index():
     json_response = requests.get('https://httpbin.org/get').json()
 
-    # return render_template('onewebpage.html', data=json_response)
     return jsonify(json_response)
+
+
+@app.route('/fail')
+def fail():
+    raise Exception
+
+
+@app.errorhandler(404)
+def error404(e):
+    msg = 'Page not found {}'.format(e)
+    app.logger.error(msg)
+    return render_template('404.html'), 404
 
 
 @app.errorhandler(500)
 def error500(e):
-    msg = f'Internal server error {e}'
+    msg = 'Internal server error {}'.format(e)
     app.logger.error(msg)
-    return msg
+    return render_template('error.html', msg=msg), 500
 
 
-@app.errorhandler(404)
-def error500(e):
-    msg = f'Page not found {e}'
+@app.errorhandler(requests.exceptions.ConnectionError)
+def connection_error(e):
+    msg = 'There was a Connection Error'
     app.logger.error(msg)
-    return msg
+    return render_template('error.html', msg=msg), 500
 
 
 @app.errorhandler(Exception)
 def ex(e):
-    msg = f'Python exception {e}'
-    app.logger.error(msg)
-    return msg
+    app.logger.error("Exception happen")
+    return render_template('error.html', msg=e.args), 500
 
 
 if __name__ == '__main__':
