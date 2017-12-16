9ee20c11db75f26f32ea664b1c97e7230fc65def
Kazimierz Sroka <kazz.sroka@gmail.com>
2017-12-16 20:55:13 +0100

    Add handlers and logging

--- nokia_homework.py
+++ nokia_homework.py
@@ -10,11 +10,31 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-
     json_response = requests.get('https://httpbin.org/get').json()
 
     # return render_template('onewebpage.html', data=json_response)
     return jsonify(json_response)
+
+
+@app.errorhandler(500)
+def error500(e):
+    msg = f'Internal server error {e}'
+    app.logger.error(msg)
+    return msg
+
+
+@app.errorhandler(404)
+def error500(e):
+    msg = f'Page not found {e}'
+    app.logger.error(msg)
+    return msg
+
+
+@app.errorhandler(Exception)
+def ex(e):
+    msg = f'Python exception {e}'
+    app.logger.error(msg)
+    return msg
 
 
 if __name__ == '__main__':
