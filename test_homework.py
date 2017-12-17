ed24c5e52c24bd1d6076b38892501ab7cf1ef95c
Kazimierz Sroka <kazz.sroka@gmail.com>
2017-12-17 16:10:53 +0100

    fixup! Add pylint

--- test_homework.py
+++ test_homework.py
@@ -1,17 +1,27 @@
+"""
+Tests for nokia homework
+"""
 import unittest
 from nokia_homework import APP
 
 
 class MyTestCase(unittest.TestCase):
+    """
+    Main test case
+    """
     def setUp(self):
         APP.testing = True
         self.app = APP.test_client()
 
     def test_response_type(self):
-        r = self.app.get('/')
+        """
+        Tests if response is a json
+        :return: None
+        """
+        response = self.app.get('/')
 
-        self.assertEqual(r.status_code, 200)
-        self.assertEqual(r.content_type, 'application/json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content_type, 'application/json')
 
 
 if __name__ == '__main__':
